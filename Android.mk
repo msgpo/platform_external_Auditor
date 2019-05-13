355aa8ae5ae5557be3ae0a16de91c49a3f48c67e
Dan Vittegleo <dan@vittegleo.com>
2019-05-13 14:44:39 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,8 @@
-LOCAL_PATH := $(call my-dir)
-
+LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Auditor
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE) 
 LOCAL_SRC_FILES := prebuilt/Auditor.apk
-
 include $(BUILD_PREBUILT)
